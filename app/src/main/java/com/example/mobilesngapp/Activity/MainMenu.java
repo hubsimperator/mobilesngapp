0f31a72253c845417a80fc0fa71fca28b34bc3f2
RGiebultowski <raf.giebultowski@gmail.com>
2020-07-17 14:29:28 +0200

    Utworzony system kalendarza do menu głównego

--- app/src/main/java/com/example/mobilesngapp/Activity/MainMenu.java
+++ app/src/main/java/com/example/mobilesngapp/Activity/MainMenu.java
@@ -1,28 +1,59 @@
 package com.example.mobilesngapp.Activity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.text.InputType;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mobilesngapp.R;
 
+import java.text.DateFormat;
+import java.util.Calendar;
+
 public class MainMenu extends AppCompatActivity {
-
-    LinearLayout linearLayout;
-    TextView dayPlanTextView;
-    TextView text2;
-    TextView text3;
-
+    DatePickerDialog picker;
+    EditText date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_taskmenu);
+        setContentView(R.layout.activity_menu);
+        date =(EditText) findViewById(R.id.dateText);
+        date.setInputType(InputType.TYPE_NULL);
+        Calendar c = Calendar.getInstance();
+        String currentDate = DateFormat.getDateInstance(DateFormat.DEFAULT).format(c.getTime());
+        date.setText(currentDate);
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar calendar = Calendar.getInstance();
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                int month = calendar.get(Calendar.MONTH);
+                int year = calendar.get(Calendar.YEAR);
+                picker = new DatePickerDialog(MainMenu.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                String newDay = String.valueOf(dayOfMonth);
+                                if (dayOfMonth <= 9){
+                                   newDay = "0" + dayOfMonth;
+                                }else{
+                                    date.setText(dayOfMonth + "." + (monthOfYear + 1) + "." + year);
+                                }
+                                if(monthOfYear < 9){
+                                    date.setText(newDay + ".0" + (monthOfYear + 1) + "." + year);
+                                }else{
+                                    date.setText(newDay + "." + (monthOfYear + 1) + "." + year);
+                                }
 
-        linearLayout = findViewById(R.id.test1);
-        dayPlanTextView = findViewById(R.id.dayPlanTextView);
-        text2 = findViewById(R.id.textView6);
-        text3 = findViewById(R.id.textView7);
+
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
     }
 }
