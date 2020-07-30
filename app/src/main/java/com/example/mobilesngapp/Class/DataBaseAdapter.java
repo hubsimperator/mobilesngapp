2d8331b584371ca179d42d6c0a3b7e47037ec2b1
RGiebultowski <raf.giebultowski@gmail.com>
2020-07-30 10:53:35 +0200

    naprawa głównego layout, dodanie godzin prac i brygad

--- app/src/main/java/com/example/mobilesngapp/Class/DataBaseAdapter.java
+++ app/src/main/java/com/example/mobilesngapp/Class/DataBaseAdapter.java
@@ -10,7 +10,10 @@ import android.widget.TextView;
 
 import com.example.mobilesngapp.R;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import static com.example.mobilesngapp.R.layout.item_workplan;
 
@@ -27,16 +30,44 @@ public class DataBaseAdapter extends ArrayAdapter<Job> {
             convertView = LayoutInflater.from(getContext()).inflate(item_workplan, parent, false);
         }
         TextView workNameText = (TextView) convertView.findViewById(R.id.dataBaseTextView);
+        TextView startTimeWorkTextView = (TextView) convertView.findViewById(R.id.startTimeWorkTextView);
+        TextView endTimeWorkTextView = (TextView) convertView.findViewById(R.id.endTimeWorkTextView);
+        TextView squadTextView = (TextView) convertView.findViewById(R.id.squadTextView);
+        //String delimiter = "\\s+";
+        String startJob = job.JobMinStart;
+        String endJob = job.JobEnd;
+        Date startJobDate = null;
+        try {
+            startJobDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(startJob);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        String timeStartJob = new SimpleDateFormat("HH:mm:ss").format(startJobDate);
+
+        Date endJobDate = null;
+        try {
+            endJobDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(endJob);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        String timeEndJob = new SimpleDateFormat("HH:mm:ss").format(endJobDate);
+
         // pokolorowanie danych dla odpowiednich statusów
 
         if (job.StatusId == 501){
             workNameText.setBackgroundColor(Color.GREEN);
+            /*startTimeWorkTextView.setBackgroundColor(Color.GREEN);
+            endTimeWorkTextView.setBackgroundColor(Color.GREEN);
+            squadTextView.setBackgroundColor(Color.GREEN);*/
         }else if(job.StatusId == 550){
             workNameText.setBackgroundColor(Color.YELLOW);
         }else if (job.StatusId == 520){
             workNameText.setBackgroundColor(Color.RED);
         }
-        workNameText.setText(job.Desig + " \n " + job.NBR);
+        startTimeWorkTextView.setText(timeStartJob);
+        endTimeWorkTextView.setText(timeEndJob);
+        workNameText.setText(job.Desig);
+        squadTextView.setText(job.NBR);
         return convertView;
     }
 
